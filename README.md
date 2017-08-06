911a3aee4b790b0e514535ba3f77e1cc949b2b97
Matthew Muscat <muscat_mat@hotmail.com>
2017-08-06 16:00:03 +0200

    Include initial code base

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-vmi-event-naive-detector
+# VMI Event Naive-Hawk Detector
+
+Event Based Basic VMI Prototype Tool
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Matthew Muscat** - *Initial work* - [muscatmat](https://github.com/muscatmat)
+
+See also the list of [contributors](https://github.com/vmi-event-naive-detector/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+
---
+++ naive-hawk.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <signal.h>
+
+#include <libvmi/libvmi.h>
+#include <libvmi/events.h>
+
+static int interrupted = 0;
+static void close_handler(int sig){
+    interrupted = sig;
+}
+
+int main(int argc, char **argv)
+{
+    printf("Naive Event Hawk Program Initiated!\n");
+
+    if(argc != 2){
+        fprintf(stderr, "Usage: naive-hawk <Guest VM Name>\n");
+        return 1;
+    }
+
+    // Init variables
+    vmi_instance_t vmi;
+
+    // Setup signal action handling
+    struct sigaction act;
+    act.sa_handler = close_handler;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGHUP,  &act, NULL);
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGINT,  &act, NULL);
+    sigaction(SIGALRM, &act, NULL);
+
+    char *vm_name = argv[1];
+    
+    // Initialize the libvmi library.
+    if (VMI_FAILURE ==
+        vmi_init(&vmi, VMI_XEN, (void*)vm_name, VMI_INIT_DOMAINNAME | VMI_INIT_EVENTS, NULL, NULL))
+    {
+        printf("Failed to init LibVMI library.\n");
+        return 2;
+    }
+    printf("LibVMI initialise succeeded!\n");
+
+    // Pause vm for consistent memory access
+    if (VMI_SUCCESS != vmi_pause_vm(vmi)) {
+        printf("Failed to pause VM\n");
+        cleanup()
+        return 3;
+    }
+
+    // Modes: Identification (m) / Monitoring (m)
+    // e.g <MODE> <STRUCT_TYPE> <STRUCT_OFFSET> <FIELD_NAME>
+   char ch;
+   do
+   {
+        char mode;
+        char struct_type[100];
+        unsigned int addr_offset;
+        char struct_member[100];
+
+        printf("Enter VMI Information request in format: <MODE> <STRUCT_TYPE> <STRUCT_OFFSET> <FIELD_NAME>\n");
+        int input_length = scanf("%c %s %x %s", &mode, struct_type, &addr_offset, struct_member);
+        if (input_length != 4)
+            break;
+
+        // MM - TODO: RETRIVE DATA
+
+        // PRINT DATA
+
+        printf("Information entered: %c %s 0x%x %s\n", mode, struct_type, addr_offset, struct_member);
+    } while ((ch = getchar()) != EOF || ch != '\n' || !interrupted);
+
+    cleanup();
+
+    printf("\nNaive Event Hawk-Eye Program Ended!\n");
+    return 0;
+}
+
+void cleanup(){
+    
+    vmi_resume_vm(vmi);
+
+    // Perform cleanup of libvmi instance
+    vmi_destroy(vmi);
+}
